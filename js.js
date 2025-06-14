f56dfed07409d72a2629520d72f9768cceb030c9
Gopimgl <gopimaniganga@gmail.com>
2025-06-14 17:07:34 +0530

    jsmodifing

--- js.js
+++ js.js
@@ -1,24 +1,28 @@
-{/*
-2. Find the Largest of 3 Numbers
-3. Write a Function to Check Prime Number
-4.Find Factorial of a Number Using Loop
-5.Reverse a String Without Using Built-in Method
-6. Find the Sum of Digits in a Number
-7.Write a program to reverse a number.
-8.Check whether a number is a palindrome.
-9.Find the Sum of All Even Numbers (1 to n)
-10.Find the Power of a Number
-12.Count Digits in a Number
-13.Find the Largest Digit in a Number*/}
-let a=12321;
-let z=a.toString()
-let b=z.split("");
-let i=0;
-let s= b.map((arr)=>{
-    console.log(arr.length);
-     if(arr[arr]===arr[arr.length-1-i]) {
-   
+function g(n){
+    let p=false;
+    if(n<=1){
+        return "not prime";
     }
-});
+    else if(n===2){
+        return "prime";
+    }
+    else{
+       for(let i=2;i<Math.sqrt(n);i++){
+        if(n/i==0){
+            p=true;
+        }
+        else{
+            p=false;
+        }
+       }
+       
+    }
+    if(p){
+        console.log("it is not  prime");
+    }
+    else{
+        console.log("it is prime");
+    }
 
-
+};
+g(17);
