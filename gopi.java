5c38ec024dcb2b0b9bd5ffafab51da1f043ebe02
Gopimgl <gopimaniganga@gmail.com>
2025-07-05 23:28:39 +0530

    modifing

--- gopi.java
+++ gopi.java
@@ -1,4 +1,4 @@
-import  java.util.*;
+/*import  java.util.*;
 public class gopi{
    int[] g(int a[]){
 
@@ -19,6 +19,28 @@ return a;
         gopi gp=new gopi();
         int x[]=gp.g(new int[]{1,2,3,9});
         System.out.println(Arrays.toString(x));
-        
-}
+*/
+/*public class gopi{
+    public static void main(String[] args){
+     String a="zbcd";
+        char b=a.charAt(0);
+        int c=(int)b;
+System.out.println(c);*/
+import java.util.*;
+public class gopi{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int[] a={1,2,3,4,5};
+       int [] b={11,12,13,14};
+       System.out.println(a);
+       System.out.println(b);
+       a=b;
+       a[2]=20;
+       System.out.println(Arrays.toString(a));
+       System.out.println(Arrays.toString(b));
+       System.out.println("enter the array numbers");
+        a=new int[4];
+        System.out.println(a);
+        System.out.println(Arrays.toString(a));
+    }
 }
